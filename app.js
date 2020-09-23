2f445c4235a43742780f10e99833466ef4925a21
busmleon <leon.schoolmail@web.de>
2020-09-23 15:41:58 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
-var http = require("http");
-http.createServer(function (request, response) {
-    // Send the HTTP header 
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+require('dotenv/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const keycloak = require('./config/keycloak-config').initKeycloak();
+const routerDB = require('./routes/db');
+const session = require('express-session');
 
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(3000);
+// Constants
+const PORT = process.env.BACKEND_HOST_PORT;
+const HOST = process.env.BACKEND_HOST_IP;
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:3000/');
+// App
+const app = express();
+
+app.use(session({
+    secret: process.env.KEYCLOAK_CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    secure: true,
+    store: keycloak.store
+}));
+app.use(keycloak.middleware());
+
+//Konvertiert Token-Bodys in JSON
+app.use(bodyParser.json());
+
+var corsOption = {
+    origin: process.env.FRONTEND_URL,
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOption));
+
+//Seiten
+//Ruft alle /db Seiten auf
+app.use('/db', routerDB);
+
+//Hört Webadresse und Port ab
+app.listen(PORT, HOST);
+
